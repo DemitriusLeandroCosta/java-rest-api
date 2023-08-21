bca5c1d04fcaeb36310fea33cdbe0b14a59eb609
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2023-08-21 18:28:43 -0300

    first commit

---
+++ datapick/src/main/java/com/datapick/DatapickApplication.java
@@ -0,0 +1,13 @@
+package com.datapick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatapickApplication.class, args);
+	}
+
+}
---
+++ datapick/src/test/java/com/datapick/DatapickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datapick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatapickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
